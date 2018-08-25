fae9fd4164e399d04a1b74632c7a6c3d4db1ca3e
Juliet Antipolo <j.antipolo@yahoo.com>
2018-08-25 11:46:27 -0400

    made changes to bookcontract.java and bookprovider.java

--- app/src/main/java/com/example/android/bookstore/data/BookContract.java
+++ app/src/main/java/com/example/android/bookstore/data/BookContract.java
@@ -1,11 +1,33 @@
 package com.example.android.bookstore.data;
 
 import android.provider.BaseColumns;
+import android.net.Uri;
 
+public final class BookContract {
 
-public class BookContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // give it an empty constructor.
+    private BookContract() {}
 
-
+    /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.android.bookstore";
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_BOOKS = "books";
 
 
 
@@ -13,6 +35,8 @@ public class BookContract {
     /* Inner class that defines the table contents of the location table */
     public static final class BookEntry implements BaseColumns {
 
+        /** The content URI to access the pet data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_BOOKS);
 
         // Table name
         public static final String TABLE_NAME = "books";
