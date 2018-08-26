7e7a5636bea65a645463df9962dae58efc4bc09e
Juliet Antipolo <j.antipolo@yahoo.com>
2018-08-26 13:08:58 -0400

    added cursor adapter and listview

--- app/src/main/java/com/example/android/bookstore/CatalogActivity.java
+++ app/src/main/java/com/example/android/bookstore/CatalogActivity.java
@@ -12,6 +12,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.ListView;
+
 
 import com.example.android.bookstore.data.BookDbHelper;
 import com.example.android.bookstore.data.BookContract.BookEntry;
@@ -86,69 +88,15 @@ public class CatalogActivity extends AppCompatActivity {
                null, // selection criteria
                null); // the sort order for the returned rows
 
-        TextView displayView = (TextView) findViewById(R.id.text_view_pet);
+// Find the ListView which will be populated with the book data
+        ListView bookListView = (ListView) findViewById(R.id.list);
 
-        try {
+        //Setup an adapter to create a list item for each row of book data in the Cursor.
+        BookCursorAdapter adapter = new BookCursorAdapter(this, cursor);
 
-            // Create a header in the TextView that looks like this:
-            //
-            // The bookstore table contains <number of rows in Cursor> bookstore.
-            // _id - name - breed - gender - weight
+        // Attach the adapter to the ListView
+        bookListView.setAdapter(adapter);
 
-            //
-
-            // In the while loop below, iterate through the rows of the cursor and display
-            // the information from each column in this order.
-
-            displayView.setText("The books table contains " + cursor.getCount() + " books.\n\n");
-            displayView.append(BookEntry._ID + " - " +
-                    BookEntry.COLUMN_BOOK_PRODUCT_NAME + " - " +
-                    BookEntry.COLUMN_BOOK_PRICE + " - " +
-                    BookEntry.COLUMN_BOOK_QUANTITY + " - " +
-                    BookEntry.COLUMN_BOOK_SUPPLIER_NAME + " - " +
-                    BookEntry.COLUMN_BOOK_SUPPLIER_PHONE + "\n");
-            // Figure out the index of each column
-            int idColumnIndex = cursor.getColumnIndex(BookEntry._ID);
-            int nameColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_PRODUCT_NAME);
-            int priceColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_PRICE);
-            int quantityColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_QUANTITY);
-            int supplierNameColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_SUPPLIER_NAME);
-            int supplierPhoneColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_SUPPLIER_PHONE);
-            // Iterate through all the returned rows in the cursor
-            while (cursor.moveToNext()) {
-                // Use that index to extract the String or Int value of the word
-                // at the current row the cursor is on.
-                int currentID = cursor.getInt(idColumnIndex);
-                String currentName = cursor.getString(nameColumnIndex);
-                int currentPrice = cursor.getInt(priceColumnIndex);
-                int currentQuantity = cursor.getInt(quantityColumnIndex);
-                String currentSupplierName = cursor.getString(supplierNameColumnIndex);
-                String currentSupplierPhone = cursor.getString(supplierPhoneColumnIndex);
-
-
-                if (currentSupplierName.isEmpty()) {
-                    currentSupplierName = "Unknown supplier";
-                }
-
-                if (currentSupplierPhone.isEmpty()) {
-                    currentSupplierPhone = "";
-                }
-
-                // Display the values from each column of the current row in the cursor in the TextView
-                displayView.append(("\n" + currentID + " - " +
-                        currentName + " - " +
-                        "$ " + currentPrice + " - " +
-                        "Qty: " + currentQuantity + " - " +
-                        currentSupplierName + " - " +
-                        currentSupplierPhone));
-            }
-
-
-        } finally {
-            // Always close the cursor when you're done reading from it. This releases all its
-            // resources and makes it invalid.
-            cursor.close();
-        }
     }
 
 
