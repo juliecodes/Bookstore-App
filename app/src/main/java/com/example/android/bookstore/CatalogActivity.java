f41cf2fffd48ca39ab1cf4363f763aeffae1c967
Juliet Antipolo <j.antipolo@yahoo.com>
2018-08-27 07:51:32 -0400

    added empty view

--- app/src/main/java/com/example/android/bookstore/CatalogActivity.java
+++ app/src/main/java/com/example/android/bookstore/CatalogActivity.java
@@ -47,7 +47,15 @@ public class CatalogActivity extends AppCompatActivity {
         // and pass the context, which is the current activity.
         // mDbHelper = new BookDbHelper(this);
 
-        displayDatabaseInfo();
+        // displayDatabaseInfo();
+
+
+        // Find the ListView which will be populated with the pet data
+        ListView bookListView = (ListView) findViewById(R.id.list);
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        bookListView.setEmptyView(emptyView);
+
     }
 
     @Override
