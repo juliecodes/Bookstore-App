ff7912ee708476beabf190066c573abfa9496503
Juliet Antipolo <j.antipolo@yahoo.com>
2018-08-27 10:27:27 -0400

    load existing pet in EditorActivity.java

--- app/src/main/java/com/example/android/bookstore/EditorActivity.java
+++ app/src/main/java/com/example/android/bookstore/EditorActivity.java
@@ -31,6 +31,12 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.database.Cursor;
+
+
 
 import com.example.android.bookstore.data.BookDbHelper;
 import com.example.android.bookstore.data.BookContract.BookEntry;
@@ -39,7 +45,13 @@ import com.example.android.bookstore.data.BookDbHelper;
 /**
  * Allows user to create a new book or edit an existing one.
  */
-public class EditorActivity extends AppCompatActivity {
+public class EditorActivity extends AppCompatActivity implements
+        LoaderManager.LoaderCallbacks<Cursor>  {
+
+    /** Identifier for the pet data loader */
+    private static final int EXISTING_BOOK_LOADER = 0;
+    /** Content URI for the existing pet (null if it's a new pet) */
+    private Uri mCurrentBookUri;
 
     /** EditText field to enter the book's name */
     private EditText mNameEditText;
@@ -68,16 +80,21 @@ public class EditorActivity extends AppCompatActivity {
         // Examine the intent that was used to launch this activity.
         // in order to figure out if we're creating a new book or editing an existing one.
         Intent intent = getIntent();
-        Uri currentBookUri = intent.getData();
+        mCurrentBookUri = intent.getData();
+
 
         // If the intent DOES NOT contain a book content URI, then we know that we are
         // creating a new book.
-        if (currentBookUri == null) {
+        if (mCurrentBookUri == null) {
             // This is a book, so change the app bar to say "Add a Book"
             setTitle(getString(R.string.editor_activity_title_new_book));
         } else {
             // Otherwise this is an existing book, so change app bar to say "Edit Book"
             setTitle(getString(R.string.editor_activity_title_edit_book));
+
+            // Initialize a loader to read the pet data from the database
+            // and display the current values in the editor
+            getLoaderManager().initLoader(EXISTING_BOOK_LOADER, null, this);
         }
 
         // Find all relevant views that we will need to read user input from
@@ -169,5 +186,66 @@ public class EditorActivity extends AppCompatActivity {
                 return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Since the editor shows all pet attributes, define a projection that contains
+        // all columns from the pet table
+        String[] projection = {
+                BookEntry._ID,
+                BookEntry.COLUMN_BOOK_PRODUCT_NAME,
+                BookEntry.COLUMN_BOOK_PRICE,
+                BookEntry.COLUMN_BOOK_QUANTITY,
+                BookEntry.COLUMN_BOOK_SUPPLIER_NAME,
+                BookEntry.COLUMN_BOOK_SUPPLIER_PHONE };
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                mCurrentBookUri,         // Query the content URI for the current pet
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+        // Bail early if the cursor is null or there is less than 1 row in the cursor
+        if (cursor == null || cursor.getCount() < 1) {
+            return;
+        }
+        // Proceed with moving to the first row of the cursor and reading data from it
+        // (This should be the only row in the cursor)
+        if (cursor.moveToFirst()) {  // Find the columns of pet attributes that we're interested in
+            int nameColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_PRODUCT_NAME);
+            int priceColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_PRICE);
+            int quantityColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_QUANTITY);
+            int supplierNameColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_SUPPLIER_NAME);
+            int supplierPhoneColumnIndex = cursor.getColumnIndex(BookEntry.COLUMN_BOOK_SUPPLIER_PHONE);
+
+            // Extract out the value from the Cursor for the given column index
+            String name = cursor.getString(nameColumnIndex);
+            int price = cursor.getInt(priceColumnIndex);
+            int quantity = cursor.getInt(quantityColumnIndex);
+            String supplierName = cursor.getString(supplierNameColumnIndex);
+            String supplierPhone = cursor.getString(supplierPhoneColumnIndex);
+            // Update the views on the screen with the values from the database
+            mNameEditText.setText(name);
+            mPriceEditText.setText(Integer.toString(price));
+            mQuantityEditText.setText(Integer.toString(quantity));
+            mSupplierNameEditText.setText(supplierName);
+            mSupplierPhoneEditText.setText(supplierPhone);
+
+        }
+    }
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+            // If the loader is invalidated, clear out all the data from the input fields.
+            mNameEditText.setText("");
+            mPriceEditText.setText("");
+            mQuantityEditText.setText("");
+            mSupplierNameEditText.setText("");
+            mSupplierPhoneEditText.setText("");
     }
 }
